56e03b62083fb4ee37733be89843662e528b836f
ko-suke24 <kosuke0322@gmail.com>
2021-11-07 19:35:00 +0900

    Initial Commit

---
+++ Custom_Side_Menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom_Side_Menu
+//
+//  Created by 佐藤航輔 on 2021/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom_Side_Menu/Custom_Side_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Side_MenuApp.swift
+//  Custom_Side_Menu
+//
+//  Created by 佐藤航輔 on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Side_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
